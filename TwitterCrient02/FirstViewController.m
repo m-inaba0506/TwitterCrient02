d308dc948e2d0830272bb2bc665699c8a02f1251
inaba masaya <inabamasaya0506@gmail.com>
2013-05-15 20:00:58 +0900

    Initial Commit

---
+++ TwitterCrient02/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  TwitterCrient02
+//
+//  Created by inaba masaya on 13/05/15.
+//  Copyright (c) 2013年 inaba masaya. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
